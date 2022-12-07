176ecaf38d7303741248e0c202fd99590d963a9d
sirio95 <sirio.iasevoli@gmail.com>
2022-12-07 15:35:40 +0100

    azioni npm

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default{
+    data(){
+        return{
+            title: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{title}}</h1>
+</template>
+
+<style>
+
+</style>
